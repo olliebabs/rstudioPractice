8c15014ca73a5bd626b7b5e55d96266c16e5ee82
lvb19bnh <lvb19bnh@bangor.ac.uk>
2022-09-13 10:24:20 +0100

    testing

--- prac.R
+++ prac.R
@@ -1,8 +1,11 @@
-<<<<<<< HEAD
 #this is the main branch:
 print('hello')
-=======
+
 #This is the dev brach:
 print('test1')
 print('test2')
->>>>>>> devBranch
+ devBranch
+#should be all done
+ 
+ 
+ 
