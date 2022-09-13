be84b235e153c7e70acffa427cb1166c48519d6b
lvb19bnh <lvb19bnh@bangor.ac.uk>
2022-09-13 10:30:20 +0100

    new change for merge 2

--- prac.R
+++ prac.R
@@ -1,11 +1,10 @@
-#this is the main branch:
-print('hello')
-
+#this is the main branch dev continued:
+print('hellooooo')
+ptint('helloNew')
 #This is the dev brach:
 print('test1')
 print('test2')
  devBranch
 #should be all done
- 
  
  
